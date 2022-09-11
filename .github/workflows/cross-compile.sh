e18bae404bd21eac864d0caa56d8496dcaed1829
Spike^ekipS <spikeekips@gmail.com>
2022-09-12 08:51:31 +0900

    fixed; cross compile; remove android

--- .github/workflows/cross-compile.sh
+++ .github/workflows/cross-compile.sh
@@ -1,12 +1,50 @@
 #!/bin/bash
 
 set -e
+set -x
 
 GOVERSION=$(go version | cut -d " " -f 3 | cut -b 3-6)
 
-supported=( "android/arm64" "darwin/amd64" "darwin/arm64" "dragonfly/amd64" "freebsd/386" "freebsd/arm" "freebsd/arm64" "freebsd/amd64" "illumos/amd64" "linux/386" "linux/amd64" "linux/arm" "linux/arm64" "linux/mips" "linux/mips64" "linux/mips64le" "linux/mipsle" "linux/ppc64" "linux/ppc64le" "linux/riscv64" "linux/s390x" "netbsd/386" "netbsd/amd64" "netbsd/arm" "netbsd/arm64" "openbsd/386" "openbsd/amd64" "openbsd/arm" "openbsd/arm64" "solaris/amd64" "windows/386" "windows/amd64" "windows/arm" "windows/arm64" )
+supported=(
+    "darwin/amd64"
+    "darwin/arm64"
+    "dragonfly/amd64"
+    "freebsd/386"
+    "freebsd/arm"
+    "freebsd/arm64"
+    "freebsd/amd64"
+    "illumos/amd64"
+    "linux/386"
+    "linux/amd64"
+    "linux/arm"
+    "linux/arm64"
+    "linux/mips"
+    "linux/mips64"
+    "linux/mips64le"
+    "linux/mipsle"
+    "linux/ppc64"
+    "linux/ppc64le"
+    "linux/riscv64"
+    "linux/s390x"
+    "netbsd/386"
+    "netbsd/amd64"
+    "netbsd/arm"
+    "netbsd/arm64"
+    "openbsd/386"
+    "openbsd/amd64"
+    "openbsd/arm"
+    "openbsd/arm64"
+    "solaris/amd64"
+    "windows/386"
+    "windows/amd64"
+    "windows/arm"
+    "windows/arm64"
+)
 
-races=( "linux/amd64" )
+races=(
+    "linux/amd64"
+    "linux/arm64"
+)
 
 for dist in ${supported[@]}; do
 	goos=$(echo $dist | cut -d "/" -f1)
@@ -27,7 +65,7 @@ for dist in ${races[@]}; do
 	echo "$dist"
 
     b=/tmp/main-${goos}-${goarch}
-	GOOS=$goos GOARCH=$goarch go build -race -o $b example/*.go
+	CGO_ENABLED=1 GOOS=$goos GOARCH=$goarch go build -race -o $b example/*.go
 
 	rm $b
 done
