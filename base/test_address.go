87e57e01505b512e152e4388d87ce4f0e074a268
Spike^ekipS <spikeekips@gmail.com>
2023-02-19 09:42:15 +0900

    SimpleAddress added for testing

--- base/test_address.go
+++ base/test_address.go
@@ -8,3 +8,11 @@ import "github.com/spikeekips/mitum/util"
 func RandomAddress(prefix string) Address {
 	return NewStringAddress(prefix + util.UUID().String())
 }
+
+func SimpleAddress(name string) Address {
+	if len(name) < 3 {
+		name = name + util.UUID().String()
+	}
+
+	return NewStringAddress(name)
+}
