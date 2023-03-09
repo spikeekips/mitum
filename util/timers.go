f57bd4c4e0305597a56320407c4966fadbb5d93d
Spike^ekipS <spikeekips@gmail.com>
2023-03-10 03:19:47 +0900

    SimpleTimers added

--- util/timers.go
+++ util/timers.go
@@ -3,6 +3,7 @@ package util
 import (
 	"context"
 	"sync"
+	"time"
 
 	"github.com/pkg/errors"
 )
@@ -297,4 +298,260 @@ func (ts *Timers) traverse(callback func(Timer), ids []TimerID) error {
 	wg.Wait()
 
 	return nil
+}
+
+type SimpleTimer struct {
+	intervalFunc  func(uint64) time.Duration
+	callback      func(context.Context, uint64) (bool, error)
+	whenRemoved   func()
+	expiredLocked *Locked[time.Time]
+	id            TimerID
+	called        uint64
+	sync.RWMutex
+}
+
+func NewSimpleTimer(
+	id TimerID,
+	intervalFunc func(uint64) time.Duration,
+	callback func(context.Context, uint64) (bool, error),
+	whenRemoved func(),
+) *SimpleTimer {
+	if whenRemoved == nil {
+		whenRemoved = func() {} //revive:disable-line:modifies-parameter
+	}
+
+	return &SimpleTimer{
+		id:            id,
+		intervalFunc:  intervalFunc,
+		callback:      callback,
+		whenRemoved:   whenRemoved,
+		expiredLocked: NewLocked(time.Time{}),
+	}
+}
+
+func (t *SimpleTimer) isExpired() bool {
+	i, _ := t.expiredLocked.Value()
+
+	return time.Now().After(i)
+}
+
+func (t *SimpleTimer) run(ctx context.Context) (bool, error) {
+	t.Lock()
+	defer t.Unlock()
+
+	if i := t.intervalFunc(t.called); i < 1 {
+		return false, nil
+	}
+
+	defer func() {
+		t.expiredLocked.SetValue(time.Now().Add(t.intervalFunc(t.called + 1)))
+		t.called++
+	}()
+
+	switch keep, err := t.callback(ctx, t.called); {
+	case err != nil || !keep:
+		return keep, err
+	default:
+		return true, nil
+	}
+}
+
+type SimpleTimers struct {
+	*ContextDaemon
+	timers   LockedMap[TimerID, *SimpleTimer]
+	ids      []TimerID
+	interval time.Duration
+}
+
+func NewSimpleTimers(size int64, interval time.Duration) (*SimpleTimers, error) {
+	if interval < 1 {
+		return nil, errors.Errorf("too narrow interval, %v", interval)
+	}
+
+	timers, err := NewLockedMap(TimerID(""), (*SimpleTimer)(nil), size)
+	if err != nil {
+		return nil, err
+	}
+
+	ts := &SimpleTimers{
+		timers:   timers,
+		interval: interval,
+	}
+
+	ts.ContextDaemon = NewContextDaemon(ts.start)
+
+	return ts, nil
+}
+
+func NewSimpleTimersFixedIDs(size int64, interval time.Duration, ids []TimerID) (*SimpleTimers, error) {
+	ts, err := NewSimpleTimers(size, interval)
+	if err != nil {
+		return nil, err
+	}
+
+	ts.ids = ids
+
+	return ts, nil
+}
+
+func (ts *SimpleTimers) Stop() error {
+	if err := ts.ContextDaemon.Stop(); err != nil {
+		return err
+	}
+
+	_ = ts.removeAllTimers()
+
+	ts.timers.Close()
+
+	return nil
+}
+
+func (ts *SimpleTimers) New(
+	id TimerID,
+	intervalFunc func(uint64) time.Duration,
+	callback func(context.Context, uint64) (bool, error),
+) (bool, error) {
+	return ts.NewTimer(NewSimpleTimer(id, intervalFunc, callback, nil))
+}
+
+func (ts *SimpleTimers) NewTimer(timer *SimpleTimer) (bool, error) {
+	if len(ts.ids) > 0 {
+		if InSlice(ts.ids, timer.id) < 0 {
+			return false, errors.Errorf("unknown timer id, %q", timer.id)
+		}
+	}
+
+	interval := timer.intervalFunc(0)
+	if interval < 1 {
+		return false, nil
+	}
+
+	_ = timer.expiredLocked.SetValue(time.Now().Add(interval))
+
+	_ = ts.timers.SetValue(timer.id, timer)
+
+	return true, nil
+}
+
+func (ts *SimpleTimers) StopAllTimers() error {
+	_ = ts.removeAllTimers()
+
+	return nil
+}
+
+func (ts *SimpleTimers) StopTimers(ids []TimerID) error {
+	for i := range ids {
+		_ = ts.removeTimer(ids[i])
+	}
+
+	return nil
+}
+
+func (ts *SimpleTimers) start(ctx context.Context) error {
+	ticker := time.NewTicker(ts.interval)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-ctx.Done():
+			return ctx.Err()
+		case <-ticker.C:
+			if err := ts.iterate(ctx); err != nil {
+				return err
+			}
+		}
+	}
+}
+
+func (ts *SimpleTimers) iterate(ctx context.Context) error {
+	semsize := int64(ts.timers.Len())
+	if semsize > 333 { //nolint:gomnd //...
+		semsize = 333
+	}
+
+	wk := NewDistributeWorker(ctx, semsize, nil)
+
+	removech := make(chan TimerID, ts.timers.Len())
+
+	ts.timers.Traverse(func(id TimerID, timer *SimpleTimer) bool {
+		tr := timer
+
+		if !timer.isExpired() {
+			return true
+		}
+
+		timer.expiredLocked.SetValue(time.Now().Add(time.Hour * 3333)) //nolint:gomnd // big enough
+
+		_ = wk.NewJob(func(ctx context.Context, _ uint64) error {
+			if keep, err := tr.run(ctx); err != nil || !keep { // NOTE remove
+				removech <- tr.id
+			}
+
+			return nil
+		})
+
+		return true
+	})
+	wk.Done()
+
+	go func() {
+		defer wk.Close()
+
+		if err := wk.Wait(); err != nil {
+			return
+		}
+
+		close(removech)
+
+		for id := range removech {
+			_ = ts.removeTimer(id)
+		}
+	}()
+
+	return nil
+}
+
+func (ts *SimpleTimers) removeAllTimers() int64 {
+	var removed int64
+
+	for {
+		ids := make([]TimerID, 333)
+
+		var i int
+		ts.timers.Traverse(func(id TimerID, _ *SimpleTimer) bool {
+			ids[i] = id
+			i++
+
+			return i != len(ids)
+		})
+
+		ids = ids[:i]
+		if len(ids) < 1 {
+			break
+		}
+
+		var c int64
+
+		for i := range ids {
+			if ts.removeTimer(ids[i]) {
+				c++
+			}
+		}
+
+		removed += c
+	}
+
+	return removed
+}
+
+func (ts *SimpleTimers) removeTimer(id TimerID) bool {
+	removed, _ := ts.timers.Remove(id, func(timer *SimpleTimer, found bool) error {
+		if found {
+			timer.whenRemoved()
+		}
+
+		return nil
+	})
+
+	return removed
 }
--- util/timers_test.go
+++ util/timers_test.go
@@ -2,6 +2,9 @@ package util
 
 import (
 	"context"
+	"fmt"
+	"strings"
+	"sync"
 	"testing"
 	"time"
 
@@ -249,4 +252,725 @@ func (t *testTimers) TestStop() {
 
 func TestTimers(t *testing.T) {
 	suite.Run(t, new(testTimers))
+}
+
+type testSimpleTimers struct {
+	suite.Suite
+}
+
+func (t *testSimpleTimers) TestNew() {
+	ts, _ := NewSimpleTimers(1, time.Millisecond)
+
+	t.NoError(ts.Start(context.Background()))
+	defer ts.Stop()
+
+	closech := make(chan struct{}, 1)
+	called := make(chan string, 4)
+	ts.NewTimer(NewSimpleTimer(
+		TimerID("a"),
+		func(i uint64) time.Duration {
+			if i == 3 {
+				go func() {
+					<-time.After(time.Millisecond * 333)
+					closech <- struct{}{}
+				}()
+
+				return 0
+			}
+
+			return time.Millisecond * 3
+		},
+		func(_ context.Context, i uint64) (bool, error) {
+			called <- fmt.Sprintf("a-%d", i)
+
+			return true, nil
+		},
+		nil,
+	))
+
+	var received []string
+
+end:
+	for {
+		select {
+		case <-closech:
+			break end
+		case i := <-called:
+			t.T().Log("called:", i)
+
+			received = append(received, i)
+		}
+	}
+
+	t.Equal([]string{"a-0", "a-1", "a-2"}, received)
+}
+
+func (t *testSimpleTimers) TestVariableInterval() {
+	ts, _ := NewSimpleTimers(1, time.Millisecond)
+
+	t.NoError(ts.Start(context.Background()))
+	defer ts.Stop()
+
+	interval := time.Millisecond * 33
+
+	closech := make(chan struct{}, 1)
+	called := make(chan [2]interface{}, 4)
+
+	started := time.Now()
+
+	ts.NewTimer(NewSimpleTimer(
+		TimerID("a"),
+		func(i uint64) time.Duration {
+			if i == 3 {
+				go func() {
+					<-time.After(time.Millisecond * 333)
+					closech <- struct{}{}
+				}()
+
+				return 0
+			}
+
+			it := interval * time.Duration(i+1)
+
+			return it
+		},
+		func(_ context.Context, i uint64) (bool, error) {
+			called <- [2]interface{}{
+				i,
+				time.Now(),
+			}
+
+			return true, nil
+		},
+		nil,
+	))
+
+	var rids []uint64
+	var rnows []time.Time
+
+end:
+	for {
+		select {
+		case <-closech:
+			break end
+		case i := <-called:
+			rid := i[0].(uint64)
+			rnow := i[1].(time.Time)
+
+			rids = append(rids, rid)
+			rnows = append(rnows, rnow)
+		}
+	}
+
+	t.Equal([]uint64{0, 1, 2}, rids)
+
+	prev := started
+	for i := range rnows {
+		now := rnows[i]
+
+		sub := now.Sub(prev)
+
+		base := interval * time.Duration(i+1)
+		diff := interval
+
+		t.T().Log("received interval:", i, sub, base, base-diff, base+diff)
+
+		t.True(sub > base-diff)
+		t.True(sub < base+diff)
+
+		prev = now
+	}
+}
+
+func (t *testSimpleTimers) TestMultiple() {
+	ts, _ := NewSimpleTimers(1, time.Millisecond)
+
+	t.NoError(ts.Start(context.Background()))
+	defer ts.Stop()
+
+	closech := make(chan struct{}, 1)
+	called := make(chan [2]interface{}, 10)
+
+	ainterval := time.Millisecond * 30
+	astarted := time.Now()
+	ts.NewTimer(NewSimpleTimer(
+		TimerID("a"),
+		func(i uint64) time.Duration {
+			if i == 3 {
+				return 0
+			}
+
+			return ainterval
+		},
+		func(_ context.Context, i uint64) (bool, error) {
+			called <- [2]interface{}{
+				fmt.Sprintf("a-%d", i),
+				time.Now(),
+			}
+
+			return true, nil
+		},
+		nil,
+	))
+
+	bstarted := time.Now()
+
+	binterval := time.Millisecond * 60
+	ts.NewTimer(NewSimpleTimer(
+		TimerID("b"),
+		func(i uint64) time.Duration {
+			if i == 3 {
+				go func() {
+					<-time.After(time.Millisecond * 333)
+					closech <- struct{}{}
+				}()
+
+				return 0
+			}
+
+			return binterval
+		},
+		func(_ context.Context, i uint64) (bool, error) {
+			called <- [2]interface{}{
+				fmt.Sprintf("b-%d", i),
+				time.Now(),
+			}
+
+			return true, nil
+		},
+		nil,
+	))
+
+	for range make([]int, 33) {
+		ts.NewTimer(NewSimpleTimer(
+			TimerID(UUID().String()),
+			func(i uint64) time.Duration {
+				return time.Millisecond * 11
+			},
+			func(_ context.Context, i uint64) (bool, error) {
+				called <- [2]interface{}{
+					fmt.Sprintf("c-%d", i),
+					time.Now(),
+				}
+
+				return true, nil
+			},
+			nil,
+		))
+	}
+
+	var aids, bids []string
+	var anows, bnows []time.Time
+
+end:
+	for {
+		select {
+		case <-closech:
+			break end
+		case i := <-called:
+			id := i[0].(string)
+			now := i[1].(time.Time)
+
+			if strings.HasPrefix(id, "a-") {
+				aids = append(aids, id)
+				anows = append(anows, now)
+			}
+
+			if strings.HasPrefix(id, "b-") {
+				bids = append(bids, id)
+				bnows = append(bnows, now)
+			}
+		}
+	}
+
+	t.Equal([]string{"a-0", "a-1", "a-2"}, aids)
+	t.Equal([]string{"b-0", "b-1", "b-2"}, bids)
+
+	aprev := astarted
+	for i := range anows {
+		now := anows[i]
+
+		sub := now.Sub(aprev)
+
+		diff := ainterval
+
+		t.T().Log("received interval:", i, sub, ainterval, ainterval-diff, ainterval+diff)
+
+		t.True(sub > ainterval-diff)
+		t.True(sub < ainterval+diff)
+
+		aprev = now
+	}
+
+	bprev := bstarted
+	for i := range bnows {
+		now := bnows[i]
+
+		sub := now.Sub(bprev)
+
+		diff := binterval
+
+		t.T().Log("received interval:", i, sub, binterval, binterval-diff, binterval+diff)
+
+		t.True(sub > binterval-diff)
+		t.True(sub < binterval+diff)
+
+		bprev = now
+	}
+}
+
+func (t *testSimpleTimers) TestStopTimers() {
+	ts, _ := NewSimpleTimers(1, time.Millisecond)
+
+	t.NoError(ts.Start(context.Background()))
+	defer ts.Stop()
+
+	closech := make(chan struct{}, 1)
+	removech := make(chan struct{})
+
+	called := make(chan string, 4)
+	ts.NewTimer(NewSimpleTimer(
+		TimerID("a"),
+		func(i uint64) time.Duration {
+			return time.Millisecond * 33
+		},
+		func(_ context.Context, i uint64) (bool, error) {
+			called <- fmt.Sprintf("a-%d", i)
+
+			if i == 2 {
+				go func() {
+					removech <- struct{}{}
+				}()
+			}
+
+			return true, nil
+		},
+		nil,
+	))
+
+	go func() {
+		<-removech
+
+		_ = ts.StopTimers([]TimerID{"a"})
+
+		<-time.After(time.Millisecond * 333)
+		closech <- struct{}{}
+	}()
+
+	var received []string
+
+end:
+	for {
+		select {
+		case <-closech:
+			break end
+		case i, notclosed := <-called:
+			if !notclosed {
+				break end
+			}
+
+			t.T().Log("called:", i)
+
+			received = append(received, i)
+		}
+	}
+
+	t.Equal([]string{"a-0", "a-1", "a-2"}, received)
+}
+
+func (t *testSimpleTimers) TestStop() {
+	ts, _ := NewSimpleTimers(1, time.Millisecond)
+
+	t.NoError(ts.Start(context.Background()))
+	defer ts.Stop()
+
+	closech := make(chan struct{}, 1)
+	removech := make(chan struct{})
+
+	called := make(chan string, 4)
+	ts.NewTimer(NewSimpleTimer(
+		TimerID("a"),
+		func(i uint64) time.Duration {
+			return time.Millisecond * 33
+		},
+		func(_ context.Context, i uint64) (bool, error) {
+			called <- fmt.Sprintf("a-%d", i)
+
+			if i == 2 {
+				go func() {
+					removech <- struct{}{}
+				}()
+			}
+
+			return true, nil
+		},
+		nil,
+	))
+
+	go func() {
+		<-removech
+
+		_ = ts.Stop()
+
+		<-time.After(time.Millisecond * 333)
+		closech <- struct{}{}
+	}()
+
+	var received []string
+
+end:
+	for {
+		select {
+		case <-closech:
+			break end
+		case i, notclosed := <-called:
+			if !notclosed {
+				break end
+			}
+
+			t.T().Log("called:", i)
+
+			received = append(received, i)
+		}
+	}
+
+	t.Equal([]string{"a-0", "a-1", "a-2"}, received)
+}
+
+func (t *testSimpleTimers) TestMultipleStop() {
+	ts, _ := NewSimpleTimers(1, time.Millisecond)
+
+	t.NoError(ts.Start(context.Background()))
+	defer ts.Stop()
+
+	closech := make(chan struct{}, 1)
+	removech := make(chan struct{})
+	called := make(chan string, 10)
+
+	ts.NewTimer(NewSimpleTimer(
+		TimerID("a"),
+		func(i uint64) time.Duration {
+			return time.Millisecond * 33
+		},
+		func(_ context.Context, i uint64) (bool, error) {
+			called <- fmt.Sprintf("a-%d", i)
+
+			return true, nil
+		},
+		nil,
+	))
+
+	ts.NewTimer(NewSimpleTimer(
+		TimerID("b"),
+		func(i uint64) time.Duration {
+			return time.Millisecond * 66
+		},
+		func(_ context.Context, i uint64) (bool, error) {
+			called <- fmt.Sprintf("b-%d", i)
+
+			if i > 3 {
+				go func() {
+					removech <- struct{}{}
+				}()
+			}
+
+			return true, nil
+		},
+		nil,
+	))
+
+	go func() {
+		<-removech
+
+		_ = ts.Stop()
+
+		<-time.After(time.Millisecond * 333)
+		closech <- struct{}{}
+	}()
+
+end:
+	for {
+		select {
+		case <-closech:
+			break end
+		case _, notclosed := <-called:
+			if !notclosed {
+				break end
+			}
+		}
+	}
+}
+
+func (t *testSimpleTimers) TestStopOther() {
+	ts, _ := NewSimpleTimers(1, time.Millisecond)
+
+	t.NoError(ts.Start(context.Background()))
+	defer ts.Stop()
+
+	closech := make(chan struct{}, 1)
+	called := make(chan string, 10)
+
+	ts.NewTimer(NewSimpleTimer(
+		TimerID("a"),
+		func(i uint64) time.Duration {
+			if i == 5 {
+				return 0
+			}
+
+			return time.Millisecond * 33
+		},
+		func(_ context.Context, i uint64) (bool, error) {
+			called <- fmt.Sprintf("a-%d", i)
+
+			if i == 2 {
+				ts.StopTimers([]TimerID{"b"})
+			}
+
+			return true, nil
+		},
+		nil,
+	))
+
+	ts.NewTimer(NewSimpleTimer(
+		TimerID("b"),
+		func(i uint64) time.Duration {
+			return time.Millisecond * 33
+		},
+		func(_ context.Context, i uint64) (bool, error) {
+			called <- fmt.Sprintf("b-%d", i)
+
+			return true, nil
+		},
+		nil,
+	))
+
+	go func() {
+		<-time.After(time.Second)
+		closech <- struct{}{}
+	}()
+
+	var aids, bids []string
+
+	closecalled := time.After(time.Second * 2)
+	var closeonce sync.Once
+end:
+	for {
+		select {
+		case <-closech:
+			break end
+		case <-closecalled:
+			closeonce.Do(func() {
+				go func() {
+					_ = ts.Stop()
+
+					<-time.After(time.Millisecond * 333)
+					closech <- struct{}{}
+				}()
+			})
+		case i, notclosed := <-called:
+			if !notclosed {
+				break end
+			}
+
+			if strings.HasPrefix(i, "a-") {
+				aids = append(aids, i)
+			} else {
+				bids = append(bids, i)
+			}
+		}
+	}
+
+	t.Equal([]string{"a-0", "a-1", "a-2", "a-3", "a-4"}, aids)
+	t.True(len(bids) < 5)
+}
+
+func (t *testSimpleTimers) TestLongRunning() {
+	ts, _ := NewSimpleTimers(1, time.Millisecond)
+
+	t.NoError(ts.Start(context.Background()))
+	defer ts.Stop()
+
+	closech := make(chan struct{}, 1)
+	called := make(chan [2]interface{}, 10)
+
+	ainterval := time.Millisecond * 30
+	astarted := time.Now()
+	ts.NewTimer(NewSimpleTimer(
+		TimerID("a"),
+		func(i uint64) time.Duration {
+			if i == 3 {
+				return 0
+			}
+
+			return ainterval
+		},
+		func(_ context.Context, i uint64) (bool, error) {
+			called <- [2]interface{}{
+				fmt.Sprintf("a-%d", i),
+				time.Now(),
+			}
+
+			if i == 1 {
+				<-time.After(time.Millisecond * 333)
+			}
+
+			return true, nil
+		},
+		nil,
+	))
+
+	bstarted := time.Now()
+
+	binterval := time.Millisecond * 30
+	ts.NewTimer(NewSimpleTimer(
+		TimerID("b"),
+		func(i uint64) time.Duration {
+			return binterval
+		},
+		func(_ context.Context, i uint64) (bool, error) {
+			called <- [2]interface{}{
+				fmt.Sprintf("b-%d", i),
+				time.Now(),
+			}
+
+			return true, nil
+		},
+		nil,
+	))
+
+	var aids, bids []string
+	var anows, bnows []time.Time
+
+	closecalled := time.After(time.Second * 2)
+
+	var closeonce sync.Once
+end:
+	for {
+		select {
+		case <-closech:
+			break end
+		case <-closecalled:
+			closeonce.Do(func() {
+				go func() {
+					_ = ts.Stop()
+
+					<-time.After(time.Millisecond * 333)
+					closech <- struct{}{}
+				}()
+			})
+		case i, notclosed := <-called:
+			if !notclosed {
+				break end
+			}
+
+			id := i[0].(string)
+			now := i[1].(time.Time)
+
+			if strings.HasPrefix(id, "a-") {
+				aids = append(aids, id)
+				anows = append(anows, now)
+			} else {
+				bids = append(bids, id)
+				bnows = append(bnows, now)
+			}
+		}
+	}
+
+	t.Equal([]string{"a-0", "a-1", "a-2"}, aids)
+	t.True(len(bids) > 10)
+
+	aprev := astarted
+	for i := range anows {
+		now := anows[i]
+
+		sub := now.Sub(aprev)
+
+		diff := ainterval
+		if i == 2 {
+			diff = time.Millisecond*333 + ainterval
+		}
+
+		t.T().Log("received a interval:", i, sub, ainterval, ainterval-diff, ainterval+diff)
+
+		t.True(sub > ainterval-diff)
+		t.True(sub < ainterval+diff)
+
+		aprev = now
+	}
+
+	bprev := bstarted
+	for i := range bnows {
+		now := bnows[i]
+
+		sub := now.Sub(bprev)
+
+		diff := binterval
+
+		t.T().Log("received a interval:", i, sub, binterval, binterval-diff, binterval+diff)
+
+		t.True(sub > ainterval-diff)
+		t.True(sub < ainterval+diff)
+
+		bprev = now
+	}
+}
+
+func (t *testSimpleTimers) TestUnknownID() {
+	ts, _ := NewSimpleTimersFixedIDs(1, time.Millisecond, []TimerID{"a"})
+
+	t.NoError(ts.Start(context.Background()))
+	defer ts.Stop()
+
+	t.Run("add ok", func() {
+		added, err := ts.NewTimer(NewSimpleTimer(
+			TimerID("a"),
+			func(i uint64) time.Duration {
+				return time.Millisecond * 33
+			},
+			func(_ context.Context, i uint64) (bool, error) {
+				return true, nil
+			},
+			nil,
+		))
+
+		t.True(added)
+		t.NoError(err)
+	})
+
+	t.Run("add again", func() {
+		added, err := ts.NewTimer(NewSimpleTimer(
+			TimerID("a"),
+			func(i uint64) time.Duration {
+				return time.Millisecond * 33
+			},
+			func(_ context.Context, i uint64) (bool, error) {
+				return true, nil
+			},
+			nil,
+		))
+
+		t.True(added)
+		t.NoError(err)
+	})
+
+	t.Run("unknown", func() {
+		added, err := ts.NewTimer(NewSimpleTimer(
+			TimerID("b"),
+			func(i uint64) time.Duration {
+				return time.Millisecond * 33
+			},
+			func(_ context.Context, i uint64) (bool, error) {
+				return true, nil
+			},
+			nil,
+		))
+
+		t.False(added)
+		t.Error(err)
+		t.ErrorContains(err, "unknown timer")
+	})
+}
+
+func TestSimpleTimers(t *testing.T) {
+	suite.Run(t, new(testSimpleTimers))
 }
