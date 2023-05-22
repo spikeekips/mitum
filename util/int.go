01c4148d449bdf23403e23317c48a31de9a4735d
Spike^ekipS <spikeekips@gmail.com>
2023-05-22 15:41:37 +0900

    For sorting consistency, use BigEndian

--- util/int.go
+++ util/int.go
@@ -17,11 +17,11 @@ func int64ToBytes(i int64, endian binary.ByteOrder) []byte {
 }
 
 func Int64ToBytes(i int64) []byte {
-	return int64ToBytes(i, binary.LittleEndian)
+	return int64ToBytes(i, binary.BigEndian)
 }
 
 func Int64ToBigBytes(i int64) []byte {
-	return int64ToBytes(i, binary.LittleEndian)
+	return int64ToBytes(i, binary.BigEndian)
 }
 
 func bytesToInt64(b []byte, endian binary.ByteOrder) (int64, error) {
@@ -36,19 +36,19 @@ func bytesToInt64(b []byte, endian binary.ByteOrder) (int64, error) {
 }
 
 func BytesToInt64(b []byte) (int64, error) {
-	return bytesToInt64(b, binary.LittleEndian)
+	return bytesToInt64(b, binary.BigEndian)
 }
 
 func BigBytesToInt64(b []byte) (int64, error) {
-	return bytesToInt64(b, binary.LittleEndian)
+	return bytesToInt64(b, binary.BigEndian)
 }
 
 func Uint64ToBytes(i uint64) []byte {
-	return uint64ToBytes(i, binary.LittleEndian)
+	return uint64ToBytes(i, binary.BigEndian)
 }
 
 func Uint64ToBigBytes(i uint64) []byte {
-	return uint64ToBytes(i, binary.LittleEndian)
+	return uint64ToBytes(i, binary.BigEndian)
 }
 
 func uint64ToBytes(i uint64, endian binary.ByteOrder) []byte {
@@ -62,7 +62,7 @@ func BytesToUint64(b []byte) (uint64, error) {
 	var i uint64
 	buf := bytes.NewReader(b)
 
-	if err := binary.Read(buf, binary.LittleEndian, &i); err != nil {
+	if err := binary.Read(buf, binary.BigEndian, &i); err != nil {
 		return 0, errors.Wrap(err, "invalid uint64 bytes")
 	}
 
@@ -72,21 +72,21 @@ func BytesToUint64(b []byte) (uint64, error) {
 func Float64ToBytes(i float64) []byte {
 	bt := math.Float64bits(i)
 	b := make([]byte, 8)
-	binary.LittleEndian.PutUint64(b, bt)
+	binary.BigEndian.PutUint64(b, bt)
 
 	return b
 }
 
 func UintToBytes(i uint) []byte {
 	b := new(bytes.Buffer)
-	_ = binary.Write(b, binary.LittleEndian, uint64(i))
+	_ = binary.Write(b, binary.BigEndian, uint64(i))
 
 	return b.Bytes()
 }
 
 func Uint8ToBytes(i uint8) []byte {
 	b := new(bytes.Buffer)
-	_ = binary.Write(b, binary.LittleEndian, i)
+	_ = binary.Write(b, binary.BigEndian, i)
 
 	return b.Bytes()
 }
