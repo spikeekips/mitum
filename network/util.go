355cbe9fd61bfefbfb3dce0d775412307c3c03f1
Spike^ekipS <spikeekips@gmail.com>
2023-08-24 01:31:59 +0900

    EnsureRead(network/util.go) removed

--- network/util.go
+++ network/util.go
@@ -1,45 +1,10 @@
 package network
 
 import (
-	"context"
-	"io"
 	"net"
 
 	"github.com/pkg/errors"
 )
-
-func EnsureRead(ctx context.Context, r io.Reader, b []byte) (int, error) {
-	if len(b) < 1 {
-		return 0, nil
-	}
-
-	var n int
-
-	for {
-		select {
-		case <-ctx.Done():
-			return n, errors.WithStack(ctx.Err())
-		default:
-			l := make([]byte, len(b)-n)
-
-			i, err := r.Read(l)
-
-			switch {
-			case err == nil:
-			case !errors.Is(err, io.EOF):
-				return n, errors.WithStack(err)
-			}
-
-			n += i
-
-			copy(b[len(b)-len(l):], l)
-
-			if n == len(b) || errors.Is(err, io.EOF) {
-				return n, errors.WithStack(err)
-			}
-		}
-	}
-}
 
 func IsValidAddr(s string) error {
 	if len(s) < 1 {
